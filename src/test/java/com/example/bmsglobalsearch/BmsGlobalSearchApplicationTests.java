9d925fbe4814c74c903c38beae7983180e1d6cb9
Deepak Saini <deepak1307saini@gmail.com>
2023-03-17 14:51:07 +0530

    intializaton

---
+++ src/test/java/com/example/bmsglobalsearch/BmsGlobalSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmsglobalsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsGlobalSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
